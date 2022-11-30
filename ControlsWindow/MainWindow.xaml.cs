e881c2b422793fd653c6eda40d664b46e2ba4527
Sergey Evseev <sevseev@gmail.com>
2022-11-30 21:22:16 +0300

    GridView

---
+++ ControlsWindow/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            var b = (Button) sender;
+            MessageBox.Show(b.Content.ToString());
+        }
+
+        private void Mouse_Move(object sender, RoutedEventArgs e)
+        {
+            //var b = (Button)sender;
+            //MessageBox.Show(b.Content.ToString());
+        }
+    }
+}
